c12c3faafa87201374afa3e5d932f3c8005ba5bc
atothrp <atoth20@cogeco.ca>
2016-08-04 09:38:05 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,86 +1,48 @@
-class nginx  (
-#  $root = undef,
-  $package = $nginx::params::package,
-  $owner   = $nginx::params::owner,
-  $group   = $nginx::params::group,
-  $docroot = hiera('nginx::docroot'),
-#  $default_docroot = $nginx::params::default_docroot,
-  $confdir = $nginx::params::confdir,
-  $logdir  = $nginx::params::logdir,
-  $user    = $nginx::params::user,
-) inherits nginx::params {
-#  case $::osfamily {
-#    'redhat' : {
-#      $package = 'nginx'
-#      $owner   = 'root'
-#      $group   = 'root'
-#      if $root != undef {$docroot = $root} else {$docroot = '/var/www'}
-#      $confdir = '/etc/nginx'
-#      $logdir  = '/var/log/nginx'
-#      $user    = 'nginx'
-#    }
-#    'debian' : {
-#      $package = 'nginx'
-#      $owner   = 'root'
-#      $group   = 'root'
-#      if $root != undef {$docroot = $root} else {$docroot = '/var/www'}
-#      $confdir = '/etc/nginx'
-#      $logdir  = '/var/log/nginx'
-#      $user    = 'www-data'
-#    }
-#    'windows' : {
-#      $package = 'nginx-service'
-#      $owner   = 'Administrator'
-#      $group   = 'Administrators'
-#      if $root != undef {$docroot = $root} else {$docroot = 'C:/ProgramData/nginx/html'}
-#      $confdir = 'C:/ProgramData/nginx'
-#      $logdir  = 'C:/ProgramData/nginx/logs'
-#      $user    = 'nobody'
-#    }
-#    default   : {
-#      fail("Module ${module_name} is not supported on ${::osfamily}")
-#    }
-#  }
- 
-# $docroot = $root? {
-#   undef => $default_docroot,
-#   default => $root,
-#  }
-  
-  package { $package:
+class nginx {
+
+  package { 'nginx':
     ensure => present,
   }
-  File {
-    owner => $owner,
-    group => $group,
-    mode => '0664',
-  }
-  file { $docroot:
+  
+  file { '/var/www':
     ensure => directory,
-    mode   => '0775',
+    owner  => 'root',
+    group  => 'root',
   }
-  file { "${docroot}/index.html":
+  
+  file { '/var/www/index.html':
     ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0664',
     source => 'puppet:///modules/nginx/index.html',
   }
-  file { "${confdir}/nginx.conf":
-    ensure  => file,
-    content  => template('nginx/nginx.conf.erb'),
-    require => Package[$package],
-    notify  => Service['nginx'],
+  
+  file { 'default.conf':
+    ensure => file,
+    path   => '/etc/nginx/conf.d/default.conf',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0664',
+    source => 'puppet:///modules/nginx/default.conf',
   }
-  file { "${confdir}/conf.d":
-    ensure => directory,
-    mode   => '0775',
+  
+  file { 'nginx.conf':
+    ensure => file,
+    path   => '/etc/nginx/nginx.conf',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0664',
+    source => 'puppet:///modules/nginx/nginx.conf',
   }
-  file { "${confdir}/conf.d/default.conf":
-    ensure  => file,
-    content  => template('nginx/default.conf.erb'),
-    require => Package[$package],
-    notify  => Service['nginx'],
-  }
+  
   service { 'nginx':
     ensure    => running,
     enable    => true,
+    subscribe => [ File['default.conf'], File['nginx.conf'] ],
   }
+
 }
+
+    Contact GitHub API Training Shop Blog About 
+
